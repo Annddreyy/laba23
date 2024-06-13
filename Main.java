28c44f2393d7cfbd033f4f19422e8cdedf44799b
Annddreyy <109622384+Annddreyy@users.noreply.github.com>
2024-06-13 18:53:53 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,40 +1,36 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
         Volt220 device220 = new Device220Volt();
         Volt380 device380 = new Device380Volt();
 
-        device220.printVolt220();
-        device380.printVolt380();
-
         Phone phone220 = new Phone(device220);
-        PhoneAdapter phone380 = new PhoneAdapter(device380);
+        PhoneAdapter phoneAdapter = new PhoneAdapter(device380);
+        Phone phone380 = new Phone(phoneAdapter);
 
-        phone220.charge();
-        phone380.charge();
+        phone220.chargePhone();
+        phone380.chargePhone();
     }
 }
 
 interface Volt220{
-    void printVolt220();
+    void charge();
 }
 
 interface Volt380{
-    void printVolt380();
+    void work();
 }
 
 class Device220Volt implements Volt220{
-    public void printVolt220(){
-        System.out.println(220);
+    public final int volt = 220;
+    public void charge(){
+        System.out.println(volt);
     }
 }
 
 class Device380Volt implements Volt380{
-    public void printVolt380(){
-        System.out.println(380);
+    public final int volt = 380;
+    public void work(){
+        System.out.println(volt);
     }
 }
 
@@ -43,20 +39,18 @@ class Phone{
     public Phone(Volt220 device){
         this.device = device;
     }
-
-    public void charge(){
-        device.printVolt220();
+    public void chargePhone() {
+        device.charge();
     }
 }
 
 
-class PhoneAdapter{
+class PhoneAdapter implements Volt220{
     private Volt380 device;
     public PhoneAdapter(Volt380 device){
         this.device = device;
     }
-
     public void charge(){
-        device.printVolt380();
+        device.work();
     }
 }
